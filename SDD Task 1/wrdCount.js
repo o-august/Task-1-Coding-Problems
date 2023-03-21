371ea24fe30ab14e0df756e0707a5a2cb5c47471
o-august <aleksander.augustynowicz@gmail.com>
2023-03-21 18:38:52 +1100

    removed files and fixed defects

--- SDD Task 1/wrdCount.js
+++ SDD Task 1/wrdCount.js
@@ -1,9 +1,19 @@
+document.querySelector("#mybutton").addEventListener("click", wordCounterhtml);
+
 function wordCounter(sentence){
-    var splitText = sentence.split(" ")
+    var splitText = sentence.trim().split(" ")
     var elementNumber = splitText.length
     return elementNumber
 }
-
+    
 var wordCount= wordCounter("hello world")
 
 console.log("word count:"+wordCount)
+function wordCounterhtml() {
+    var message
+    var sentence = document.querySelector("#text").value;
+    message = "the amount of words in your sentence is " + wordCounter(sentence)
+    document.querySelector("#message").innerHTML = message 
+  }
+  
+  
